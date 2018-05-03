6f0076037c5d4dadf5b318bcd83dc5d817b998a2
yzhbankov <yzhbankov@gmail.com>
2018-05-03 19:16:19 +0300

    add and get users

--- server/users/handlers.js
+++ server/users/handlers.js
@@ -1,13 +1,39 @@
+const error = require('http-errors');
+const bcrypt = require('bcrypt');
+const ObjectId = require('mongodb').ObjectID;
 const db = require('./../../db');
 
-async function getUsers() {
+async function findUserByEmail(email) {
     const usersCollection = db.get().collection('users');
+    const user = await usersCollection.findOne({ email });
+    return user;
+}
 
-    const users = await usersCollection.find({}).toArray();
+async function getUsers(uid = null) {
+    const usersCollection = db.get().collection('users');
+    if (!uid) {
+        return usersCollection.find({}).toArray();
+    }
+    return usersCollection.findOne({ _id: ObjectId(uid) });
+}
 
-    return users;
+async function createUser(user) {
+    const usersCollection = db.get().collection('users');
+    const userExist = await findUserByEmail(user.email);
+    console.log(userExist)
+    if (userExist) {
+        throw error(422, 'User with this email already exist');
+    }
+    const now = (new Date()).getTime();
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(user.password, saltRounds);
+
+    const result = await usersCollection.insert({ email: user.email, password: hashedPassword, admin: user.admin, created_at: now });
+
+    return result.ops[0];
 }
 
 module.exports = {
     getUsers,
+    createUser,
 };
--- server/users/router.js
+++ server/users/router.js
@@ -1,13 +1,35 @@
 const express = require('express');
 
-const { getUsers } = require('./handlers');
+const { getUsers, createUser } = require('./handlers');
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
+router.get(['/', '/:uid'], async (req, res) => {
     try {
-        const users = await getUsers();
-        res.status(200).send(users);
+        const uid = req.params.uid;
+        const query = req.query;
+        console.log('query', query);
+        if (uid) {
+            const user = await getUsers(uid);
+            res.status(200).send(user);
+        } else {
+            const users = await getUsers();
+            res.status(200).send(users);
+        }
+    } catch (err) {
+        throw err;
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const data = {
+            email: req.body.email,
+            password: req.body.email,
+            admin: req.body.admin,
+        };
+        const user = await createUser(data);
+        res.status(200).send(user);
     } catch (err) {
         throw err;
     }
