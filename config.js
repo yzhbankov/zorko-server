318160e313147e6f0ee49afc4e90ebadf2fee17f
Igor Nesterenko <igornester@gmail.com>
2018-08-19 14:37:47 +0300

    - remove env from under VSC - add github password strategy to the project

--- config.js
+++ config.js
@@ -9,6 +9,11 @@ module.exports = {
         url: process.env.MONGO_ROOT_URL,
         name: process.env.MONGO_DB_NAME,
     },
+    github: {
+        clientId: process.env.GITHUB_CLIENT_ID,
+        secret: process.env.GITHUB_CLIENT_SECRET,
+        callbackUrl: process.env.GITHUB_CLIENT_CALLBACK_URL,
+    },
     secret: process.env.SESSION_SECRET,
     jwtsecret: process.env.JWT_SECRET,
 };
