325a293287caa0b146ea0393dbb4c8f5add5e087
Igor Nesterenko <igornester@gmail.com>
2018-08-24 18:10:25 +0300

    - add redirect for logout

--- src/auth/router.js
+++ src/auth/router.js
@@ -8,7 +8,7 @@ const config = require('../config');
 const handlers = require('./handlers');
 
 // TODO: sync up with '/auth/account'
-router.post('/local/login', passport.authenticate('local', { failureRedirect: '/login' }), (req, res) => {
+router.post('/local/login', passport.authenticate('local', { failureRedirect: config.auth.zorkoWebAppUrl }), (req, res) => {
     const token = jwt.sign(req.user, config.jwtsecret, { expiresIn: '1h' });
     return res.json({ user: req.user, token });
 });
@@ -19,7 +19,7 @@ router.get('/profile', handlers.ensureAuthenticated, (req, res) => {
 
 router.get('/logout', (req, res) => {
     req.logout();
-    res.send('You are logout');
+    res.redirect(config.auth.zorkoWebAppUrl);
 });
 
 router.post('/local/sign-in', handlers.signUpHandler);
